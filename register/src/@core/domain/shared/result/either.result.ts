7a3adecf96700226700c9a183eb934cedd8bf9be
lidiagaldino <lidiagabrielly14@gmail.com>
2024-08-19 21:54:12 -0300

    feat: add result, left and right classes
    Adds classes to represent operation results, including support for representing errors and successful values.

---
+++ register/src/@core/domain/shared/result/either.result.ts
@@ -0,0 +1,5 @@
+
+import { Left } from './left.result';
+import { Right } from './right.result';
+
+export type Either<L, A> = Left<L, A> | Right<L, A>;
---
+++ register/src/@core/domain/shared/result/response.result.ts
@@ -0,0 +1,5 @@
+
+import { Either } from './either.result';
+import { Result } from './result';
+
+export type Response<T> = Either<Result<any>, Result<T>>;
