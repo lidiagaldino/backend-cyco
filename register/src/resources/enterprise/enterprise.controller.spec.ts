2c3bdc4d99bfb6805bc976c56e0cc71e019cc98b
-dev-backend <lidiagabrielly14@gmail.com>
2025-01-12 20:07:36 -0300

    test: fix enterprise controller test

--- register/src/resources/enterprise/enterprise.controller.spec.ts
+++ register/src/resources/enterprise/enterprise.controller.spec.ts
@@ -1,6 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { EnterpriseController } from './enterprise.controller';
-import { EnterpriseService } from './enterprise.service';
+import { TEnterpriseInputDTO } from '../../@core/application/dto/input/enterprise.dto.input';
+import { CreateEnterpriseUsecase } from '../../@core/application/usecases/enterprise/create-enterprise.usecase';
+import { FindEnterpriseByIdUsecase } from '../../@core/application/usecases/enterprise/find-enterprise-by-id.usecase';
+import { IEnterpriseRepository } from '../../@core/domain/repositories/enterprise.repository';
+import { IPasswordCryptography } from '../../@core/domain/services/password-cryptography.service';
+import { IValidator } from '../../@core/domain/services/validator.service';
+import { BcryptAdapter } from '../../infra/cryptography/password/bcrypt.adapter';
+import { prismaClient } from '../../infra/db/prisma';
+import { EnterpriseRepositoryImpl } from '../../infra/db/prisma/repositories/enterprise.prisma-repository';
+import { enterpriseSchema } from '../../infra/validation/yup/schemas/enterprise.schema';
+import { YupAdapter } from '../../infra/validation/yup/yup.adapter';
 
 describe('EnterpriseController', () => {
   let controller: EnterpriseController;
@@ -8,7 +18,34 @@ describe('EnterpriseController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [EnterpriseController],
-      providers: [EnterpriseService],
+      providers: [
+        { provide: YupAdapter, useClass: YupAdapter },
+        { provide: EnterpriseRepositoryImpl, useFactory: () => new EnterpriseRepositoryImpl(prismaClient) },
+        { provide: BcryptAdapter, useFactory: () => new BcryptAdapter(8) },
+        {
+          provide: CreateEnterpriseUsecase,
+          useFactory: (
+            repository: IEnterpriseRepository,
+            passwordCryptography: IPasswordCryptography,
+            validator: IValidator<TEnterpriseInputDTO>
+          ) => new CreateEnterpriseUsecase(
+            repository,
+            passwordCryptography,
+            validator,
+            enterpriseSchema
+          ),
+          inject: [EnterpriseRepositoryImpl, BcryptAdapter, YupAdapter]
+        },
+        {
+          provide: FindEnterpriseByIdUsecase,
+          useFactory: (
+            repository: IEnterpriseRepository
+          ) => new FindEnterpriseByIdUsecase(
+            repository
+          ),
+          inject: [EnterpriseRepositoryImpl]
+        }
+      ],
     }).compile();
 
     controller = module.get<EnterpriseController>(EnterpriseController);
